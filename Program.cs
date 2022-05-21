b18112ac12cbe7b07b9b1f7cd3a338549c3316e9
Mikhail Vinogradov <m89119750476@gmail.com>
2022-05-21 23:39:32 +0300

    done?

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int length = 5;
+int[] array = new int[length];
+Random rnd = new Random();
+int sum = 0;
+for (int i = 0; i < length; i++) 
+{
+    array[i] = rnd.Next(1, 100);
+    if (i % 2 != 0) 
+        sum += array[i];
+        Console.Write(array[i] + "  ");
+        
+}
+Console.WriteLine(sum);
